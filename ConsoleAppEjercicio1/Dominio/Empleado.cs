6e166530985ece54730e476affc38d490ab8b27d
Vladimir Cortés A. <vladimir.cortes@outlook.com>
2025-11-26 11:36:22 -0500

    Adicione proyecto de consola y capa de dominio

---
+++ ConsoleAppEjercicio1/Dominio/Empleado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEjercicio1.Dominio
+{
+    public class Empleado
+    {
+        //Propiedades de la clase
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Cargo { get; set; }
+        public decimal Salario { get; set; }
+
+        public override string ToString()
+        {
+            return $"[Id={Id}, Nombre={Nombre}, Cargo={Cargo}, Salario={Salario:C}]";
+        }
+    }
+}
